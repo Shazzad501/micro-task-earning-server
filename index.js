dd537cd94a6e38de41f1d455ebd9c40dcf2f8c60
Shazzad501 <assazzad501@gmail.com>
2025-02-18 23:22:46 +0600

    cors orign problem

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const port = process.env.PORT || 5000;
 
 
 // middel ware
-app.use(cors({
-  origin: '*', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 
 
@@ -56,24 +52,23 @@ async function run() {
 
 
     // verify jwt token
-    const verifyToken = (req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    
-      if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'Unauthorized Access' });
+    const verifyToken = (req, res, next)=>{
+      // console.log('Inside verify token',req.headers.authorization)
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'Unauthorized Access'})
       }
       const token = req.headers.authorization.split(' ')[1];
-    
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(401).send({ message: 'Unauthorized Access' });
+      // console.log('receive toke', token)
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          console.error('JWT Verification Error:', err);
+          return res.status(401).send({message: 'Unauthorized Access'})
         }
         req.decoded = decoded;
-        next();
-      });
-    };
-    
+        next()
+      })
+      // next()
+    }
 
 
     // user data posting api
